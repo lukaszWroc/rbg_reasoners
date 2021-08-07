8f41986d783337557b6eaa0977cfb47c1b76bfc1
Lukasz Galas <galaslukasz1998@gmail.com>
2021-08-08 01:56:40 +0200

    Optimized pentago.

--- src/pentago.hpp
+++ src/pentago.hpp
@@ -7,7 +7,7 @@
 
 #define BOARD_ROWS 6
 
-#define BOARD_SIZE BOARD_ROWS*BOARD_ROWS
+#define BOARD_SIZE BOARD_ROWS * BOARD_ROWS
 
 namespace reasoner
 {
@@ -20,26 +20,63 @@ class resettable_bitarray_stack {};
 
 struct move
 {
-  uint32_t mr, pos;
+  uint32_t mr;
+  uint32_t pos;
 
   move(void) = default;
-  constexpr move(uint32_t _mr, uint32_t _pos) : mr(_mr), pos(_pos) {};
+  constexpr move(uint32_t _mr, uint32_t _pos = 0) : mr(_mr), pos(_pos) {};
   bool operator==(const move& rhs) const;
 };
 
-constexpr std::array<move, BOARD_SIZE> fill_empty_array()
-{
-  std::array<move, BOARD_SIZE> result{};
-
-  for (uint32_t i=0;i<BOARD_SIZE;i++)
-  {
-    result[i] = move(i,i);
-  }
-
-  return result;
-}
-
 typedef uint64_t board;
+
+struct rotation
+{
+  static constexpr int mask = (1 << 9);
+
+  static constexpr std::array<uint64_t, mask> rotate_arr(std::array<int, 9> t)
+  {
+    std::array<uint64_t, mask> result{};
+
+    bool tab[9] = {};
+
+    for (int k=0;k<mask;k++)
+    {
+      for (int j=0;j<9;j++)
+      {
+        if ((1 << j) & k)
+        {
+          tab[j] = 1;
+        }
+        else
+        {
+          tab[j] = 0;
+        }
+      }
+
+      uint64_t wyn = 0;
+
+      int d=0;
+
+      for(int i=0;i<9;i++)
+      {
+        if (tab[t[i]])
+        {
+          wyn |= ((uint64_t)1<<(i+d));
+        }
+
+        if (i==2 || i == 5)
+        {
+          d += 3;
+        }
+      }
+
+      result[k] = wyn;
+    }
+
+    return result;
+  }
+};
 
 class game_state
 {
@@ -53,42 +90,96 @@ public:
 
   bool apply_any_move(resettable_bitarray_stack&);
   bool is_legal(const move& m) const;
-  bool win_condition(uint32_t pl);
-  bool rotation_win(uint32_t x);
 
 private:
   board pieces[2] = {0};
 
+  int diff[4] {0,3,18,21};
+
   bool piece_move = true;
-  bool played     = false;
   bool exit       = false;
 
-  uint32_t winner         = 1;
   uint32_t current_player = 1;
-  uint32_t last_moved     = 0;
-  uint32_t last_pos       = 0;
-
-  int rotates_cnt[4] = {0,0,0,0};
 
   uint32_t index_pos[4][9] =
     {{0,1,2,6,7,8,12,13,14}, {3,4,5,9,10,11,15,16,17},
     {18,19,20,24,25,26,30,31,32}, {21,22,23,27,28,29,33,34,35}};
 
-  uint32_t rotations[4] = {0,0,0,0};
   uint32_t variables[NUMBER_OF_VARIABLES] = {50, 50};
 
+  bool win_condition(uint32_t pl);
+  bool rotation_win();
+
   void rotate(int x, uint32_t pos, uint32_t type);
-  void rotate_right(bool *tab);
-  void rotate_left(bool *tab);
-  void rotate_right(uint32_t *tab);
-  void rotate_left(uint32_t *tab);
   void rotate_all(uint32_t idx, uint32_t type);
 
-  int get_quarter(uint32_t pos);
-  uint32_t get_pos(uint32_t pos);
+  inline uint32_t msb(const uint64_t p)
+  {
+    return (63 - __builtin_clzl(p));
+  }
 
-  static constexpr std::array<move, BOARD_SIZE> empty_array = fill_empty_array();
+  __attribute__((hot)) uint64_t inline transform(uint64_t x)
+  {
+    return ((x & 0b111) | ((x & 0b111000000) >> 3) | ((x & 0b111000000000000) >> 6));
+  }
+
   static constexpr std::array<move, 8> rotates_vec = {move(0,0), move(0,1), move(1,0), move(1,1), move(2,0), move(2,1), move(3,0), move(3,1)};
-  std::vector<move> empty;
+
+  static constexpr uint64_t quatter[4] =
+    {
+      0b000111000111000111,
+      0b111000111000111000,
+      0b000111000111000111000000000000000000,
+      0b111000111000111000000000000000000000
+    };
+    static constexpr int mask = (1 << 9);
+
+
+  static constexpr std::array<uint64_t, rotation::mask> rotate_right_arr = rotation::rotate_arr({6,3,0,7,4,1,8,5,2});
+  static constexpr std::array<uint64_t, rotation::mask> rotate_left_arr  = rotation::rotate_arr({2,5,8,1,4,7,0,3,6});
+
+  static constexpr std::array<uint64_t, 32> win_arr = []()
+  {
+    std::array<uint64_t, 32> result{};
+
+    result[0] = 0b000000010000001000000100000010000001;
+    result[1] = 0b100000010000001000000100000010000000;
+    result[2] = 0b000000100000010000001000000100000010;
+    result[3] = 0b010000001000000100000010000001000000;
+    result[4] = 0b000000000010000100001000010000100000;
+    result[5] = 0b000001000010000100001000010000000000;
+    result[6] = 0b000010000100001000010000100000000000;
+    result[7] = 0b000000000001000010000100001000010000;
+
+    uint64_t tmp = 0b000000100000100000100000100000100000;
+
+    for (int i=0;i<6;i++)
+    {
+      result[i+8] = (tmp >> i);
+    }
+
+    tmp = 0b100000100000100000100000100000000000;
+
+    for (int i=0;i<6;i++)
+    {
+      result[i+14] = (tmp >> i);
+    }
+
+    tmp = 0b000000000000000000000000000000111110;
+
+    for (int i=0;i<6;i++)
+    {
+      result[i+20] = (tmp << (i*6));
+    }
+
+    tmp = 0b000000000000000000000000000000011111;
+
+    for (int i=0;i<6;i++)
+    {
+      result[i+26] = (tmp << (i*6));
+    }
+
+    return result;
+  }();
 };
 }
