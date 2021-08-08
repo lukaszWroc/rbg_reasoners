9c5e16fe40052200342075cf133af180ca78ab68
Lukasz Galas <galaslukasz1998@gmail.com>
2021-08-08 14:22:17 +0200

    Added MONOTONIC logic to tictactoe.

--- src/tictactoe.cpp
+++ src/tictactoe.cpp
@@ -1,9 +1,7 @@
 #include "reasoner.hpp"
 
-
 namespace reasoner
 {
-
 int game_state::get_current_player(void) const
 {
   return current_player;
@@ -21,61 +19,74 @@ bool game_state::apply_any_move(resettable_bitarray_stack&)
 
 int game_state::get_monotonicity_class(void)
 {
-  return -1;
+  if (exit)
+  {
+    return -1;
+  }
+
+  return 0;
 }
 
 bool game_state::is_legal([[maybe_unused]] const move& m) const
 {
-  return false;
+  #ifdef MONOTONIC
+  return !(pieces[0] & m.mr) && !(pieces[1] & m.mr) && !exit;
+  #else
+  return -1;
+  #endif
 }
 
 void game_state::apply_move(const move &m)
 {
   pieces[current_player - 1] |= m.mr;
 
-  if (win_condition() && exit_ == 0)
+
+  #ifndef MONOTONIC
+  last_pos = m.pos;
+  #endif
+
+  if (win_arr[pieces[current_player  -1]])
   {
     variables[current_player - 1] = 100;
     variables[(current_player ^ 0b11) - 1] = 0;
-    exit_ = 1;
+    exit = 1;
   }
 
   current_player ^= 0b11;
-}
-
-bool game_state::win_condition()
-{
-  return (pieces[current_player  -1] & 0b010010010) == 0b010010010 ||
-    (pieces[current_player -1] & 0b100100100) == 0b100100100 ||
-    (pieces[current_player -1] & 0b001001001) == 0b001001001 ||
-    (pieces[current_player -1] & 0b111000000) == 0b111000000 ||
-    (pieces[current_player -1] & 0b000111000) == 0b000111000 ||
-    (pieces[current_player -1] & 0b000000111) == 0b000000111 ||
-    (pieces[current_player -1] & 0b100010001) == 0b100010001 ||
-    (pieces[current_player -1] & 0b001010100) == 0b001010100;
 }
 
 void game_state::get_all_moves(resettable_bitarray_stack&, std::vector<move>& moves)
 {
   moves.clear();
 
-  if (exit_)
+  if (exit)
   {
     return;
   }
 
-  uint32_t allMoves = (~(pieces[0] | pieces[1])) & 0x000001FF;
+  if (last_pos == not_started)
+  {
+    moves.insert(moves.begin(), std::begin(empty_arr), std::end(empty_arr));
+    #ifndef MONOTONIC
+    empty_vec.insert(empty_vec.begin(), std::begin(empty_arr), std::end(empty_arr));
+    #endif
 
-  if (allMoves == 0)
+    return;
+  }
+
+  #ifndef MONOTONIC
+  if (empty_vec.size() == 1)
   {
     return;
   }
 
-  while (allMoves)
-  {
-    uint32_t mv = msb(allMoves);
-    moves.push_back(mv);
-    allMoves ^= mv;
-  }
+  empty_vec[last_pos].mr ^= empty_vec.back().mr;
+  empty_vec.back().mr    ^= empty_vec[last_pos].mr;
+  empty_vec[last_pos].mr ^= empty_vec.back().mr;
+
+  empty_vec.pop_back();
+
+  moves.insert(moves.begin(), std::begin(empty_vec), std::end(empty_vec));
+  #endif
 }
 }
--- src/tictactoe.hpp
+++ src/tictactoe.hpp
@@ -1,11 +1,20 @@
 
+#include <array>
 #include <cstdint>
 #include <vector>
+
+#define BOARD_SIZE 9
+
+#undef MONOTONIC
 
 namespace reasoner
 {
 constexpr int NUMBER_OF_PLAYERS   = 3;
+#ifdef MONOTONIC
+constexpr int MONOTONIC_CLASSES   = 1;
+#else
 constexpr int MONOTONIC_CLASSES   = 0;
+#endif
 constexpr int NUMBER_OF_VARIABLES = 2;
 constexpr int NUMBER_OF_PIECES    = 2;
 
@@ -16,8 +25,17 @@ typedef uint32_t move_representation;
 struct move
 {
   move_representation mr;
+
+  #ifndef MONOTONIC
+  move_representation pos;
+  #endif
+
   move(void) = default;
-  move(const move_representation& mv) : mr(mv) {};
+  #ifdef MONOTONIC
+  constexpr move(const move_representation& mv) : mr(mv) {};
+  #else
+  constexpr move(const move_representation& mv, const move_representation& _pos) : mr(mv), pos(_pos) {};
+  #endif
   bool operator==(const move& rhs) const;
 };
 
@@ -25,19 +43,50 @@ class game_state
 {
   int current_player = 1;
 
-  int exit_ = 0;
+  static constexpr int not_started =  1 << (BOARD_SIZE + 1);
+  int exit = 0;
+  int last_pos = not_started;
 
   int variables[NUMBER_OF_VARIABLES] = {50, 50};
 
+  std::vector<move> empty_vec;
+
   uint32_t pieces[2] = {0, 0};
 
-  inline uint32_t msb(const uint32_t& pieces) const 
+  static constexpr std::array<bool, (1 << BOARD_SIZE)> win_arr = []()
   {
-    auto k = 31 - __builtin_clz(pieces);
-    return 1u << k;
-  }
+    std::array<bool, (1 << BOARD_SIZE)> result{};
 
-  bool win_condition();
+    for (int i=0;i<(1<<BOARD_SIZE);i++)
+    {
+      result[i] = (i & (0b010010010)) == 0b010010010 ||
+                    (i & 0b100100100) == 0b100100100 ||
+                    (i & 0b001001001) == 0b001001001 ||
+                    (i & 0b111000000) == 0b111000000 ||
+                    (i & 0b000111000) == 0b000111000 ||
+                    (i & 0b000000111) == 0b000000111 ||
+                    (i & 0b100010001) == 0b100010001 ||
+                    (i & 0b001010100) == 0b001010100;
+    }
+
+    return result;
+  }();
+
+  static constexpr std::array<move, BOARD_SIZE> empty_arr = []()
+  {
+    std::array<move, BOARD_SIZE> result{};
+
+    for (uint32_t i=0;i<BOARD_SIZE;i++)
+    {
+      #ifdef MONOTONIC
+      result[i] = move(1<<i);
+      #else
+      result[i] = move(1<<i, i);
+      #endif
+    }
+
+    return result;
+  }();
 
 public:
   int get_current_player(void) const;
